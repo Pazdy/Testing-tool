a6094f90d5c2a75ef42c7420d7c591208dbeb57a
Lukáš Pazderka <pazderka-lukas@seznam.cz>
2022-01-18 23:21:43 +0100

    Adding and editing comment and warnings

--- funcionaltesting.py
+++ funcionaltesting.py
@@ -1,17 +1,24 @@
-#importing importante libraries
+# importing importante libraries
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from excelimport import configuration
 import time
 
-#path where is stored chrome driver
+# path where is stored chrome driver
 PATH = Service(r"C:\Users\HP\PycharmProjects\Testing-tool\chrome-driver\chromedriver.exe")
 
-class Steps:
-    def actions(self):
+# class Test_case stands for logic of program (store all methods)
+
+
+class Testcase:
+    # testexecution method works with nested lists-> every nested list represents one step that needs to be processed
+    def testexcecution(self):
+        # looping over all steps in the configuration
         for step in range(len(configuration)):
+            # looping over all actions in step
             for action in configuration[step]:
+                # what action should be processed
                 if action == "Browser":
                     self.chosebrowser()
                     pass
@@ -32,53 +39,63 @@ class Steps:
                     continue
                 pass
         pass
+    # method that stands for opening wanted browser filled in config
+
     def chosebrowser(self):
+        # same looping as in test excecution
         for step in range(len(configuration)):
             for action in configuration[step]:
                 if action == "Chrome":
+                    # choose webdriver as chrome
                     self.driver = webdriver.Chrome(service=PATH)
                 else:
                     continue
                 pass
             pass
         pass
+    # method that stands for visiting web which put user to config
+
     def gourl(self):
+
+        # same looping as in test excecution
         for step in range(len(configuration)):
             for action in configuration[step]:
                 if action == "URL":
-                    self.gurl = self.driver.get(url=configuration[step][1])
+                    # redirect to web
+                    self.driver.get(url=configuration[step][1])
                     pass
                 else:
                     continue
                 pass
             pass
         pass
+    # search
+
     def searchbar(self):
         for step in range(len(configuration)):
             for action in configuration[step]:
                 if action == "Search":
-                    self.searchbox = self.driver.find_element(By.XPATH, value=configuration[step][1])
-                    self.searchbox.send_keys(configuration[step][2])
+                    self.searchbox = self.driver.find_element(By.XPATH, value=configuration[step][2])
+                    self.searchbox.send_keys(configuration[step][3])
                 else:
                     continue
+
     def button(self):
         for step in range(len(configuration)):
             for action in configuration[step]:
-
                 if action == "Button":
-                    print(configuration[step][1])
                     if configuration[step][1] == "XPath":
-                        self.click = self.driver.find_element(By.XPATH, value= configuration[step][2])
+                        self.click = self.driver.find_element(By.XPATH, value=configuration[step][2])
                         time.sleep(3)
                         self.click.click()
 
                     elif configuration[step][1] == "ID":
-                        self.click = self.driver.find_element(By.ID, value= configuration[step][2])
+                        self.click = self.driver.find_element(By.ID, value=configuration[step][2])
                         time.sleep(3)
                         self.click.click()
 
                     elif configuration[step][1] == "Class":
-                        self.click = self.driver.find_element(By.CLASS_NAME, value= configuration[step][2])
+                        self.click = self.driver.find_element(By.CLASS_NAME, value=configuration[step][2])
                         time.sleep(3)
                         self.click.click()
 
@@ -92,15 +109,8 @@ class Steps:
         pass
     pass
 
-testcase = Steps().actions()
+
+Testcase().testexcecution()
+
 
 # id(unique), name(usually unique), class(not always unique), Tag
-
-
-
-
-
-
-
-
-
