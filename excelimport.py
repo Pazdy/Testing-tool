02bddf2f15829a4f822dc26e14c0e52c19c21108
Lukáš Pazderka <pazderka-lukas@seznam.cz>
2022-02-03 23:35:24 +0100

    Function code - excel import/export ok, test_case exceptions ok, executions ok To do - import multiple excels, export results for multiple excels to one excel

--- excelimport.py
+++ excelimport.py
@@ -3,10 +3,15 @@ import pandas as pd
 #importing excel file with configuration
 df = pd.read_excel(r"C:\Users\HP\PycharmProjects\Testing-tool\excel\configuration.xlsx")
 
+#drop columns which were read as unnamed (empty in excel)
+#df.dropna(axis='rows',how='all', inplace=True)
+
 # df(configuration) putted to the nested list every step represents one column
 nlist = df.T.reset_index().values.tolist()
 
 #delete NaNs from nested list
 configuration = [[i for i in x if pd.notnull(i)] for x in nlist]
+
+
 
 
--- funcionaltesting.py
+++ funcionaltesting.py
@@ -5,144 +5,174 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from excelimport import configuration
+import pandas as pd
 import time
 
 # path where is stored chrome driver
 PATH = Service(r"C:\Users\HP\PycharmProjects\Testing-tool\chrome-driver\chromedriver.exe")
 
-# class Test_case stands for logic of program (store all methods)
-
-
+# class Testcases stands for logic of program (store all methods)
 class Testcases():
-    # testexecution method works with nested lists-> every nested list represents one step that needs to be processed
+
+    # testexecution method execute tests and also store steps which goes through and result/s of test/s
     def test_excecutions(self):
-        # looping over all steps in the configuration
-        for step in range(len(configuration)):
-            # looping over all actions in step
-            for action in configuration[step]:
-                print(action)
-                # catches step you are work with
-                self.step = step
-                # what action should be processed
-                if action == "Browser":
-                    self.chosebrowser()
-                    pass
-                elif action == "URL":
-                    self.gourl()
-                    pass
-                elif action == "Search":
-                    self.searchbar()
-                    pass
-                elif action == "Button":
-                    time.sleep(3)
-                    self.clicks()
-                    pass
-                elif action == "Select":
-                    pass
-                elif action == "Back":
-                    self.back()
-                elif action == "Forward":
-                    self.forward()
-                elif action == "Clear":
-                    self.clear()
-                elif action == "End":
+
+        # result store steps which where execute, shows in which step was error or if test was processed smoothly
+        self.result = []
+
+        # If config gonna be empty then to the result will be added comment about that
+        if configuration == []:
+            self.result.append("Konfigurace je prázdná, je potřeba vložit argumenty a parametry dle dokumentace!")
+
+        # Step "Browser" must be first in config and URL must be second step in config
+        elif configuration[0][0] == "Browser" and configuration[1][0] == "URL":
+
+            # Try to loop over config
+            try:
+
+                # Looping over configuration
+                for step in configuration:
+                    self.step = step
+                    print(step)
+                    if "BROWSER" == step[0].upper():
+                        self.result.append(self.step)
+                        self.chosebrowser()
+                        pass
+                    elif "URL" == step[0].upper():
+                        self.result.append(self.step)
+                        self.gourl()
+                        pass
+                    elif "INPUT" in step[0].upper():
+                        self.result.append(self.step)
+                        self.input_text()
+                        pass
+                    elif "BUTTON" in step[0].upper():
+                        self.result.append(self.step)
+                        self.clicks()
+                    elif "BACK" in step[0].upper():
+                        self.result.append(self.step)
+                        self.back()
+                    elif "FORWARD" in step[0].upper():
+                        self.result.append(self.step)
+                        self.forward()
+                    elif "CLEAR" in step[0].upper():
+                        self.result.append(self.step)
+                        self.clear()
+                        pass
+                    elif "END" == step[0].upper():
+                        self.result.append("Test uspěšně proveden!")
+                        self.driver.quit()
+                        break
+                    elif "WAIT" in step[0].upper():
+                        self.result.append(self.step)
+                        self.wait()
+                    elif "TITLE" in step[0].upper(): #think about this
+                        self.title()
+
+                    # If there will be step which is not recognized then else runs -> result of it saved in result var.
+                    else:
+                        self.result.append("Neznamý krok! Projdi dokumentaci pro seznam kroků a jejich parametry.")
+                        self.driver.quit()
+                        break
+
+            # If in loop gonna be any issue program will run expect code
+            except:
+                # If third step in config will be steps that cannot be processed then it will be save to result var.
+                if configuration[2][0].upper() not in ("WAIT", "BUTTON", "SEARCH", "TITLE"):
+                    self.result.append("Krok v konfiguraci musí obsahovat krok, který se dá provést! Pro nápovědu použij dokumentaci.")
                     self.driver.quit()
-                    pass
-                elif action == "Wait":
-                    self.wait()
-                elif action == "Title":
-                    self.title()
-                else:
-                    continue
-                pass
+
+                # If will be error in any step of config then it will be saved in result var.
+                elif self.step[0].upper() not in ("BUTTON", "SEARCH"):
+                    self.result.append("V kroku " + self.step[0] + " nastala chyba! Pro správné spuštění oprav chybu. Zkontroluj parametry. Pro nápovědu použij dokumentaci.")
+                    self.driver.quit()
+
+        # If first two steps will not be "Browser" and "URL" else will be execute
+        else:
+            self.result.append("Počáteční krok musí být Browser a následovat musí URL! Pro nápovědu projdi dokumentaci.")
+
+    # importing result of test/s to the excel named result_file.xlsx also runs test_execution method
+    def result_file(self):
+        self.test_excecutions()
+        self.result_import = pd.DataFrame([self.result]).to_excel(r'C:\Users\HP\PycharmProjects\Testing-tool\excel\result_file.xlsx')
         pass
 
     # method that stands for opening wanted browser filled in config
-
     def chosebrowser(self):
-        # same looping as in test excecution
-        if configuration[self.step][1] == "Chrome":
-            # choose webdriver as chrome
+        if self.step[1] == "Chrome":
             self.driver = webdriver.Chrome(service=PATH)
 
-        elif configuration[self.step][1] == "Edge":
+        elif self.step[1] == "Edge":
             self.driver = webdriver.Edge(service=PATH)
 
-        elif configuration[self.step][1] == "Firefox":
+        elif self.step[1] == "Firefox":
             self.driver = webdriver.Firefox(service=PATH)
 
-        elif configuration[self.step][1] == "Safari":
+        elif self.step[1] == "Safari":
             self.driver = webdriver.Safari(service=PATH)
-        else:
-            print("Putted wrong browser")
         pass
+
     # method that stands for visiting web which put user to config
-
     def gourl(self):
-        # redirect to web
-        self.driver.get(url=configuration[self.step][1])
+        self.driver.get(url=self.step[1])
 
-    # find element of searchbar then fill it with the string putted in config
-    def searchbar(self):
-        # find specific element on the page
+    # find element a insert input
+    def input_text(self):
         self.locators()
-        self.searchbox = self.locator
+        self.location = self.locator
+
         # put string from confid to the searchbar
-        self.searchbox.send_keys(configuration[self.step][3])
+        self.location.send_keys(self.step[3])
 
     # button method that first find element to click then click
-
     def clicks(self):
-        try:
-            self.locators()
-            self.element = self.locator
-            self.element.click()
-        except:
-            self.driver.quit()
-            print("Spatný locator")
-
-    # locators method decide according to configuration by which element gonna be used for searching
-    # and also takes value for it
-
-    def locators(self):
-        if configuration[self.step][1] == "XPath":
-            self.locator = WebDriverWait(self.driver, 3).until(
-            EC.element_to_be_clickable((By.XPATH, configuration[self.step][2])))
-
-        elif configuration[self.step][1] == "ID":
-            self.locator = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.ID, configuration[self.step][2])))
-
-        elif configuration[self.step][1] == "Class":
-            self.locator = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.CLASS_NAME, configuration[self.step][2])))
-
-        elif configuration[self.step][1] == "Link":
-            self.locator = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.LINK_TEXT, configuration[self.step][2])))
-
-        elif configuration[self.step][1] == "Name":
-            self.locator = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.NAME, configuration[self.step][2])))
-
-        elif configuration[self.step][1] == "Tag":
-            self.locator = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.TAG_NAME, configuration[self.step][2])))
-
-        elif configuration[self.step][1] == "CSS_Selector":
-            self.locator = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, configuration[self.step][2])))
-
-        elif configuration[self.step][1] == "Partial_Link":
-            self.locator = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, configuration[self.step][2])))
-        else:
-            print("Error Locators")
-    def select(self):
         self.locators()
         self.element = self.locator
-        pass
-    pass
+        try:
+            self.element.click()
+        except:
+            self.result.append("Lokátor nebyl jedinečný!")
+    # locators method decide according to configuration by which element gonna be used for searching
+    # and also takes value for it ->result of this is unique identificator on the page
+    def locators(self):
+        try:
+            if self.step[1].upper() == "XPATH":
+                self.locator = WebDriverWait(self.driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, self.step[2])))
+
+            elif self.step[1].upper() == "ID":
+                self.locator = WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.ID, self.step[2])))
+
+            elif self.step[1].upper() == "CLASS":
+                self.locator = WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, self.step[2])))
+
+            elif self.step[1].upper() == "LINK":
+                self.locator = WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.LINK_TEXT, self.step[2])))
+
+            elif self.step[1].upper() == "NAME":
+                self.locator = WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.NAME, self.step[2])))
+
+            elif self.step[1].upper() == "TAG":
+                self.locator = WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.TAG_NAME, self.step[2])))
+
+            elif self.step[1].upper() == "CSSSELECTOR":
+                self.locator = WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, self.step[2])))
+
+            elif self.step[1].upper() == "PARTLINK":
+                self.locator = WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, self.step[2])))
+            else:
+                self.result.append("Chyba v názvu lokátoru!")
+                self.driver.quit()
+        except:
+            self.result.append("Chyba v cestě lokátoru!")
+            self.driver.quit()
 
     def back(self):
         for i in range(int(configuration[self.step][1])):
@@ -157,18 +187,15 @@ class Testcases():
     pass
 
     def clear(self):
-       self.clearence = self.searchbox.clear()
+       self.clearence = self.location.clear()
 
     def wait(self):
-        self.waiting = time.sleep(configuration[self.step][1])
+        self.waiting = time.sleep(int(self.step[1]))
 
-    def title(self):
-        if configuration[self.step][1] == self.driver.title:
-            self.titleO = print("Y")
+    def check_title(self):
+        if self.step[1] == self.driver.title:
+            self.title = print("Y")
         else:
-            self.titleO = print("N")
-
-
-Testcases().test_excecutions()
+            self.title = print("N")
 
 # id(unique), name(usually unique), class(not always unique), Tag
